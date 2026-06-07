b1af92f69064ef77a662a618bcade28725e990e9
adit1 <adit10022004@gmail.com>
2026-06-08 02:38:11 +0530

    Driver kt

---
+++ src/main/kotlin/com/trackmybus/backend/entity/Driver.kt
@@ -0,0 +1,25 @@
+package com.trackmybus.backend.entity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "drivers")
+data class Driver(
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    val id : Long = 0,
+    val name : String = "",
+    @Column(unique = true)
+    val email : String = "",
+    val password : String = "",
+    val employeeID : String = "",
+    val licenseNumber  : String = "",
+
+)
