df0613c4196b03192fad531e3887dc75e6c51037
adit1 <adit10022004@gmail.com>
2026-06-14 17:26:07 +0530

    stopservice change

--- src/main/kotlin/com/trackmybus/backend/service/StopService.kt
+++ src/main/kotlin/com/trackmybus/backend/service/StopService.kt
@@ -3,41 +3,38 @@ package com.trackmybus.backend.service
 import com.trackmybus.backend.dto.SaveStopsRequest
 import com.trackmybus.backend.entity.Stop
 import com.trackmybus.backend.repository.StopRepository
-import jakarta.persistence.Id
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class StopService(
-    private val
-    StopRepository: StopRepository
+    private val stopRepository: StopRepository
 ) {
+    fun getStopsByBusId(
+        busId: Long
+    ): List<Stop> {
 
+        return stopRepository.findByBusId(busId)
+    }
+
+    @Transactional
     fun savestop(
         request: SaveStopsRequest
     ): String {
-        StopRepository.deleteByBusId(
-            request.busId
-        )
+
+        stopRepository.deleteByBusId(request.busId)
+
         request.stops.forEachIndexed { index, stopName ->
 
-            val Stop = Stop(
+            val stop = Stop(
                 busId = request.busId,
                 stopName = stopName,
                 stopOrder = index + 1
             )
-            StopRepository.save(Stop)
+
+            stopRepository.save(stop)
         }
+
         return "Stops saved successfully"
-
     }
-
-    fun getStopsByBusId(
-        busId: Long
-
-    ): List<Stop> {
-        return StopRepository.findByBusId(busId)
-
-    }
-
-
 }
